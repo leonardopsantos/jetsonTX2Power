088d7e7d953f19f24441aa176c463ca34605389c
Leonardo Pereira Santos <leonardo.santos@restinga.ifrs.edu.br>
2018-05-25 13:55:32 -0300

    Changed code style

--- jetson_tx2_power.cpp
+++ jetson_tx2_power.cpp
@@ -16,14 +16,14 @@ using namespace std;
 
 void PowerReadingValue::update(void)
 {
-	std::ifstream inFile;
-	inFile.open(this->path.c_str());
-	if (!inFile) {
-		this->value = "-1.0";
-	} else {
-		inFile >> this->value;
-		inFile.close();
-	}
+    std::ifstream inFile;
+    inFile.open(this->path.c_str());
+    if (!inFile) {
+        this->value = "-1.0";
+    } else {
+        inFile >> this->value;
+        inFile.close();
+    }
 }
 
 std::string PowerReadingValue::get_name(void)
@@ -37,297 +37,309 @@ std::string PowerReadingValue::get_value(void)
 
 PowerReadingValue::PowerReadingValue(std::string name)
 {
-	this->name = name;
-	this->path = "";
-	this->value = "";
-	this->number = 0;
+    this->name = name;
+    this->path = "";
+    this->value = "";
+    this->number = 0;
 }
 
-std::ostream& operator<< (std::ostream& stream, const PowerReadingValue& value)
+std::ostream& operator<<(std::ostream& stream, const PowerReadingValue& value)
 {
-	stream << "name:" << value.name << ", path:" << value.path << ", value:" << value.value;
-	return stream;
+    stream << "name:" << value.name << ", path:" << value.path << ", value:"
+            << value.value;
+    return stream;
 }
 
-PowerReadingVoltage::PowerReadingVoltage(std::string path, int number):PowerReadingValue("voltage")
+PowerReadingVoltage::PowerReadingVoltage(std::string path, int number) :
+        PowerReadingValue("voltage")
 {
-	std::ostringstream ss;
-	ss << number;
-	std::string s(ss.str());
+    std::ostringstream ss;
+    ss << number;
+    std::string s(ss.str());
 
-	this->name = name;
-	this->path = path+"/in_voltage"+s+"_input";
-	this->value = 0.0;
+    this->name = name;
+    this->path = path + "/in_voltage" + s + "_input";
+    this->value = 0.0;
 }
 
-PowerReadingCurrent::PowerReadingCurrent(std::string path, int number):PowerReadingValue("current")
+PowerReadingCurrent::PowerReadingCurrent(std::string path, int number) :
+        PowerReadingValue("current")
 {
-	std::ostringstream ss;
-	ss << number;
-	std::string s(ss.str());
+    std::ostringstream ss;
+    ss << number;
+    std::string s(ss.str());
 
-	this->name = name;
-	this->path = path+"/in_current"+s+"_input";
-	this->value = 0.0;
+    this->name = name;
+    this->path = path + "/in_current" + s + "_input";
+    this->value = 0.0;
 }
 
-PowerReadingRail::PowerReadingRail(std::string path, int num):voltage(path, num), current(path, num)
+PowerReadingRail::PowerReadingRail(std::string path, int num) :
+        voltage(path, num), current(path, num)
 {
-	std::ifstream inFile;
+    std::ifstream inFile;
 
-	std::ostringstream ss;
-	ss << num;
-	std::string s(ss.str());
+    std::ostringstream ss;
+    ss << num;
+    std::string s(ss.str());
 
-	inFile.open((path+"/rail_name_"+s).c_str());
-	if (!inFile) {
-		this->name = "";
-	} else {
-		inFile >> this->name;
-		inFile.close();
-	}
+    inFile.open((path + "/rail_name_" + s).c_str());
+    if (!inFile) {
+        this->name = "";
+    } else {
+        inFile >> this->name;
+        inFile.close();
+    }
 }
 
 std::string PowerReadingRail::get_name(void)
 {
-	return this->name;
+    return this->name;
 }
 
 void PowerReadingRail::update(void)
 {
-	this->voltage.update();
-	this->current.update();
+    this->voltage.update();
+    this->current.update();
 }
 
 std::string PowerReadingRail::to_csv(void)
 {
-	string s = this->voltage.get_value()+"," + this->current.get_value();
-	return s;
+    string s = this->voltage.get_value() + "," + this->current.get_value();
+    return s;
 }
 
 std::string PowerReadingRail::get_csv_header(void)
 {
-	string s = this->name+" voltage," + this->name+" current";
-	return s;
+    string s = this->name + " voltage," + this->name + " current";
+    return s;
 }
 
-std::ostream& operator<< (std::ostream& stream, const PowerReadingRail& r)
+std::ostream& operator<<(std::ostream& stream, const PowerReadingRail& r)
 {
-	stream << "name:" << r.name << endl;
-	stream << "    voltage:" << r.voltage << endl;
-	stream << "    current:" << r.current;
-	return stream;
+    stream << "name:" << r.name << endl;
+    stream << "    voltage:" << r.voltage << endl;
+    stream << "    current:" << r.current;
+    return stream;
 }
 
 PowerReadingDevice::PowerReadingDevice(std::string path)
 {
-	for (int i = 0; i < 3; ++i) {
-		rails.push_back(PowerReadingRail(path, i));
-	}
+    for (int i = 0; i < 3; ++i) {
+        rails.push_back(PowerReadingRail(path, i));
+    }
 }
 
 void PowerReadingDevice::update(void)
 {
-	unsigned int i = 0;
-	for (std::vector<PowerReadingRail>::iterator it = this->rails.begin();
-			it != this->rails.end(); ++it, ++i) {
-			it->update();
-	}
+    unsigned int i = 0;
+    for (std::vector<PowerReadingRail>::iterator it = this->rails.begin();
+            it != this->rails.end(); ++it, ++i) {
+        it->update();
+    }
 }
 
 std::string PowerReadingDevice::to_csv(void)
 {
-	std::string s;
-	for (std::vector<PowerReadingRail>::iterator it = this->rails.begin();
-			it != this->rails.end(); ++it) {
-			s += it->to_csv();
-			if( it != this->rails.end()-1 )
-				s += ",";
-	}
-	return s;
+    std::string s;
+    for (std::vector<PowerReadingRail>::iterator it = this->rails.begin();
+            it != this->rails.end(); ++it) {
+        s += it->to_csv();
+        if (it != this->rails.end() - 1)
+            s += ",";
+    }
+    return s;
 }
 
 std::string PowerReadingDevice::get_csv_header(void)
 {
-	std::string s;
-	for (std::vector<PowerReadingRail>::iterator it = this->rails.begin();
-			it != this->rails.end(); ++it) {
-			s += it->get_csv_header();
-			if( it != this->rails.end()-1 )
-				s += ",";
-	}
-	return s;
+    std::string s;
+    for (std::vector<PowerReadingRail>::iterator it = this->rails.begin();
+            it != this->rails.end(); ++it) {
+        s += it->get_csv_header();
+        if (it != this->rails.end() - 1)
+            s += ",";
+    }
+    return s;
 }
 
-std::ostream& operator<< (std::ostream& stream, const PowerReadingDevice& d)
+std::ostream& operator<<(std::ostream& stream, const PowerReadingDevice& d)
 {
-	stream << "Rails:" << endl;
+    stream << "Rails:" << endl;
 
-	unsigned int i = 0;
-	for (std::vector<PowerReadingRail>::const_iterator it = d.rails.begin();
-			it != d.rails.end(); ++it, ++i) {
-			cout << *it << endl;
-	}
-	return stream;
+    unsigned int i = 0;
+    for (std::vector<PowerReadingRail>::const_iterator it = d.rails.begin();
+            it != d.rails.end(); ++it, ++i) {
+        cout << *it << endl;
+    }
+    return stream;
 }
 
 std::vector<PowerReadingDevice> create_devices(void)
 {
-	std::vector<PowerReadingDevice> devices;
-	devices.push_back(PowerReadingDevice("/sys/devices/3160000.i2c/i2c-0/0-0040/iio_device"));
-	devices.push_back(PowerReadingDevice("/sys/devices/3160000.i2c/i2c-0/0-0041/iio_device"));
-	devices.push_back(PowerReadingDevice("/sys/devices/3160000.i2c/i2c-0/0-0042/iio_device"));
-	devices.push_back(PowerReadingDevice("/sys/devices/3160000.i2c/i2c-0/0-0043/iio_device"));
-	return devices;
+    std::vector<PowerReadingDevice> devices;
+    devices.push_back(
+            PowerReadingDevice(
+                    "/sys/devices/3160000.i2c/i2c-0/0-0040/iio_device"));
+    devices.push_back(
+            PowerReadingDevice(
+                    "/sys/devices/3160000.i2c/i2c-0/0-0041/iio_device"));
+    devices.push_back(
+            PowerReadingDevice(
+                    "/sys/devices/3160000.i2c/i2c-0/0-0042/iio_device"));
+    devices.push_back(
+            PowerReadingDevice(
+                    "/sys/devices/3160000.i2c/i2c-0/0-0043/iio_device"));
+    return devices;
 }
 
 void to_csv(std::string csv_file, std::vector<PowerReadingDevice> &devices,
-		std::vector<std::string> &xtra_fields_header,
-		std::vector<std::string> &xtra_fields_data)
+        std::vector<std::string> &xtra_fields_header,
+        std::vector<std::string> &xtra_fields_data)
 {
-int header = 0;
-std::ofstream outFile;
-char buf[8192];
-struct timeval now;
-time_t t;
-struct tm *tmp;
+    int header = 0;
+    std::ofstream outFile;
+    char buf[8192];
+    struct timeval now;
+    time_t t;
+    struct tm *tmp;
 
-	if( access( csv_file.c_str(), F_OK ) == -1 ) {
-		header = 1;
-		outFile.open(csv_file.c_str());
-	} else {
-		outFile.open(csv_file.c_str(), std::ios_base::app);
-	}
+    if (access(csv_file.c_str(), F_OK) == -1) {
+        header = 1;
+        outFile.open(csv_file.c_str());
+    } else {
+        outFile.open(csv_file.c_str(), std::ios_base::app);
+    }
 
-	if (!outFile) {
-		return;
-	}
+    if (!outFile) {
+        return;
+    }
 
-	if( header != 0 ) {
-		outFile << "date,time,";
+    if (header != 0) {
+        outFile << "date,time,";
 
-		for (vector<PowerReadingDevice>::iterator it = devices.begin();
-				it != devices.end(); ++it) {
-			outFile << it->get_csv_header() << ",";
-		}
-		for (std::vector<std::string>::iterator it = xtra_fields_header.begin();
-				it != xtra_fields_header.end(); ++it) {
-			outFile << *it;
-			if( it != xtra_fields_header.end()-1 )
-				outFile << ",";
-		}
-	}
+        for (vector<PowerReadingDevice>::iterator it = devices.begin();
+                it != devices.end(); ++it) {
+            outFile << it->get_csv_header() << ",";
+        }
+        for (std::vector<std::string>::iterator it = xtra_fields_header.begin();
+                it != xtra_fields_header.end(); ++it) {
+            outFile << *it;
+            if (it != xtra_fields_header.end() - 1)
+                outFile << ",";
+        }
+    }
 
-	update_power_values(devices);
+    update_power_values(devices);
 
-	gettimeofday(&now, NULL);
-	t = now.tv_sec;
-	tmp = localtime(&t);
+    gettimeofday(&now, NULL);
+    t = now.tv_sec;
+    tmp = localtime(&t);
 
-	memset(buf, '\0', sizeof(buf));
-	strftime(buf, sizeof(buf), "%Y-%m-%d", tmp);
-	outFile << buf;
+    memset(buf, '\0', sizeof(buf));
+    strftime(buf, sizeof(buf), "%Y-%m-%d", tmp);
+    outFile << buf;
 
-	memset(buf, '\0', sizeof(buf));
-	strftime(buf, sizeof(buf), ",%H:%M:%S", tmp);
-	outFile << buf << ":" << (now.tv_usec/1000) << ",";
+    memset(buf, '\0', sizeof(buf));
+    strftime(buf, sizeof(buf), ",%H:%M:%S", tmp);
+    outFile << buf << ":" << (now.tv_usec / 1000) << ",";
 
-	for (vector<PowerReadingDevice>::iterator it = devices.begin();
-			it != devices.end(); ++it) {
-		it->update();
-		outFile << it->to_csv() << ",";
-	}
-	for (std::vector<std::string>::iterator it = xtra_fields_data.begin();
-			it != xtra_fields_data.end(); ++it) {
-		outFile << *it;
-		if( it != xtra_fields_data.end()-1 )
-			outFile << ",";
-	}
+    for (vector<PowerReadingDevice>::iterator it = devices.begin();
+            it != devices.end(); ++it) {
+        it->update();
+        outFile << it->to_csv() << ",";
+    }
+    for (std::vector<std::string>::iterator it = xtra_fields_data.begin();
+            it != xtra_fields_data.end(); ++it) {
+        outFile << *it;
+        if (it != xtra_fields_data.end() - 1)
+            outFile << ",";
+    }
 
-	outFile.close();
+    outFile.close();
 }
 
 void to_csv(std::string csv_file, std::vector<PowerReadingDevice> &devices,
-		std::string comment)
+        std::string comment)
 {
-int header = 0;
-std::ofstream outFile;
-char buf[8192];
-struct timeval now;
-time_t t;
-struct tm *tmp;
+    int header = 0;
+    std::ofstream outFile;
+    char buf[8192];
+    struct timeval now;
+    time_t t;
+    struct tm *tmp;
 
-	if( access( csv_file.c_str(), F_OK ) == -1 ) {
-		header = 1;
-		outFile.open(csv_file.c_str());
-	} else {
-		outFile.open(csv_file.c_str(), std::ios_base::app);
-	}
+    if (access(csv_file.c_str(), F_OK) == -1) {
+        header = 1;
+        outFile.open(csv_file.c_str());
+    } else {
+        outFile.open(csv_file.c_str(), std::ios_base::app);
+    }
 
-	if (!outFile) {
-		return;
-	}
+    if (!outFile) {
+        return;
+    }
 
-	if( header != 0 ) {
-		outFile << "date,time,";
+    if (header != 0) {
+        outFile << "date,time,";
 
-		for (vector<PowerReadingDevice>::iterator it = devices.begin();
-				it != devices.end(); ++it) {
-			outFile << it->get_csv_header() << ",";
-		}
-		outFile << "comments" << endl;
-	}
+        for (vector<PowerReadingDevice>::iterator it = devices.begin();
+                it != devices.end(); ++it) {
+            outFile << it->get_csv_header() << ",";
+        }
+        outFile << "comments" << endl;
+    }
 
-	update_power_values(devices);
+    update_power_values(devices);
 
-	gettimeofday(&now, NULL);
-	t = now.tv_sec;
-	tmp = localtime(&t);
+    gettimeofday(&now, NULL);
+    t = now.tv_sec;
+    tmp = localtime(&t);
 
-	memset(buf, '\0', sizeof(buf));
-	strftime(buf, sizeof(buf), "%Y-%m-%d", tmp);
-	outFile << buf;
+    memset(buf, '\0', sizeof(buf));
+    strftime(buf, sizeof(buf), "%Y-%m-%d", tmp);
+    outFile << buf;
 
-	memset(buf, '\0', sizeof(buf));
-	strftime(buf, sizeof(buf), ",%H:%M:%S", tmp);
-	outFile << buf << ":" << (now.tv_usec/1000) << ",";
+    memset(buf, '\0', sizeof(buf));
+    strftime(buf, sizeof(buf), ",%H:%M:%S", tmp);
+    outFile << buf << ":" << (now.tv_usec / 1000) << ",";
 
-	for (vector<PowerReadingDevice>::iterator it = devices.begin();
-			it != devices.end(); ++it) {
-		it->update();
-		outFile << it->to_csv() << ",";
-	}
-	outFile << comment << endl;
+    for (vector<PowerReadingDevice>::iterator it = devices.begin();
+            it != devices.end(); ++it) {
+        it->update();
+        outFile << it->to_csv() << ",";
+    }
+    outFile << comment << endl;
 
-	outFile.close();
+    outFile.close();
 }
 
 void to_csv(std::string csv_file, std::vector<PowerReadingDevice> &devices)
 {
-	to_csv(csv_file, devices, "");
+    to_csv(csv_file, devices, "");
 }
 
 int update_power_values(std::vector<PowerReadingDevice> &devices)
 {
-	for (std::vector<PowerReadingDevice>::iterator it = devices.begin();
-			it != devices.end(); ++it) {
-		it->update();
-	}
-	return 0;
+    for (std::vector<PowerReadingDevice>::iterator it = devices.begin();
+            it != devices.end(); ++it) {
+        it->update();
+    }
+    return 0;
 }
 
 int print_values(std::vector<PowerReadingDevice> &devices)
 {
-time_t result;
+    time_t result;
 
-	result = time(NULL);
+    result = time(NULL);
 
-	printf("%.19s\n", ctime(&result));
+    printf("%.19s\n", ctime(&result));
 
-	for (std::vector<PowerReadingDevice>::iterator it = devices.begin();
-			it != devices.end(); ++it) {
-		it->update();
-		cout << *it << endl;
-	}
-	return 0;
+    for (std::vector<PowerReadingDevice>::iterator it = devices.begin();
+            it != devices.end(); ++it) {
+        it->update();
+        cout << *it << endl;
+    }
+    return 0;
 }
